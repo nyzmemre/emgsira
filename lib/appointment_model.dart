d9da0031623d0d86ed2d532271b74e44c38ca205
nyzmemre <nyz_memre_09@hotmail.com>
2024-01-30 17:20:43 +0300

    add firebase day, month and year data

--- lib/appointment_model.dart
+++ lib/appointment_model.dart
@@ -1,17 +1,19 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-
 class AppointmentModel {
   String? name;
   String? surname;
   int? phone;
-  DateTime? date;
+  int? day;
+  int? month;
+  int? year;
   bool? isActive;
 
   AppointmentModel({
     this.name,
     this.surname,
     this.phone,
-    this.date,
+    this.day,
+    this.month,
+    this.year,
     this.isActive,
   });
 
@@ -19,14 +21,18 @@ class AppointmentModel {
     String? name,
     String? surname,
     int? phone,
-    DateTime? date,
+    int? day,
+    int? month,
+    int? year,
     bool? isActive,
   }) {
     return AppointmentModel(
       name: name ?? this.name,
       surname: surname ?? this.surname,
       phone: phone ?? this.phone,
-      date: date ?? this.date,
+      day: day ?? this.day,
+      month: month ?? this.month,
+      year: year ?? this.year,
       isActive: isActive ?? this.isActive,
     );
   }
@@ -36,7 +42,9 @@ class AppointmentModel {
       'name': name,
       'surname': surname,
       'phone': phone,
-      'date': date,
+      'day': day,
+      'month': month,
+      'year': year,
       'isActive': isActive,
     };
   }
@@ -46,17 +54,20 @@ class AppointmentModel {
       name: json['name'] as String?,
       surname: json['surname'] as String?,
       phone: json['phone'] as int?,
-      date: (json['date'] as Timestamp).toDate(),
+      day: json['day'] as int?,
+      month: json['month'] as int?,
+      year: json['year'] as int?,
       isActive: json['isActive'] as bool?,
     );
   }
 
   @override
   String toString() =>
-      "PublicAppointment(name: $name,surname: $surname,phone: $phone,date: $date,isActive: $isActive)";
+      "AppointmentModel(name: $name,surname: $surname,phone: $phone,day: $day,month: $month,year: $year,isActive: $isActive)";
 
   @override
-  int get hashCode => Object.hash(name, surname, phone, date, isActive);
+  int get hashCode =>
+      Object.hash(name, surname, phone, day, month, year, isActive);
 
   @override
   bool operator ==(Object other) =>
@@ -66,6 +77,8 @@ class AppointmentModel {
               name == other.name &&
               surname == other.surname &&
               phone == other.phone &&
-              date == other.date &&
+              day == other.day &&
+              month == other.month &&
+              year == other.year &&
               isActive == other.isActive;
 }
--- lib/date_select.dart
+++ lib/date_select.dart
@@ -4,11 +4,19 @@ import 'package:emgsira/appointment_model.dart';
 import 'package:flutter/material.dart';
 import 'package:kartal/kartal.dart';
 
-class DateSelect extends StatelessWidget {
+class DateSelect extends StatefulWidget {
   const DateSelect({Key? key, required this.appointmentStream})
       : super(key: key);
   final Stream<QuerySnapshot<Map<String, dynamic>>>? appointmentStream;
 
+  @override
+  State<DateSelect> createState() => _DateSelectState();
+}
+
+class _DateSelectState extends State<DateSelect> {
+  int day=DateTime.now().day;
+  int month=DateTime.now().month;
+  int year=DateTime.now().year;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,39 +29,60 @@ class DateSelect extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                EasyDateTimeLine(initialDate: DateTime.now()),
+                EasyDateTimeLine(
+                    locale: "tr_TR",
+                    initialDate: DateTime.now(),
+                    onDateChange: (selectedDate){
+                      setState(() {
+                        day=selectedDate.day;
+                        month=selectedDate.month;
+                        year=selectedDate.year;
+                      });
+                    },
+                ),
                 context.sized.emptySizedHeightBoxLow,
                 Center(
                     child: ElevatedButton(
                         onPressed: () {}, child: Text('Randevu Ekle'))),
                 context.sized.emptySizedHeightBoxNormal,
-                StreamBuilder<QuerySnapshot>(
+                StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
                     stream: FirebaseFirestore.instance
-                        .collection('privateAppointment')
-                        .where('date', isEqualTo: DateTime.now().day)
+                        .collection('publicAppointment')
+                    .where('day', isEqualTo: day)
+                    .where('month', isEqualTo: month)
+                    .where('year', isEqualTo: year)
                         .snapshots(),
                     builder: (context, snapshot) {
-                      if (snapshot.hasData) {
-
-                        return Card(
-                          margin: context.padding.normal,
-                          child: ListTile(
-                            title: Text(snapshot.data!.docs.toString()),
-                          ),
-                        );
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        // Veri yükleniyor, yüklenene kadar bekleyin
+                        return CircularProgressIndicator();
+                      } else if (snapshot.hasError) {
+                        // Hata durumunda hatayı gösterin
+                        return Text('Error: ${snapshot.error}');
+                      } else if (snapshot.data == null || snapshot.data!.docs.isEmpty) {
+                        // Veri yoksa veya boşsa
+                        return Text('Randevu Bulunmamaktadır.');
                       } else {
-                        return Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            EasyDateTimeLine(initialDate: DateTime.now()),
-                            context.sized.emptySizedHeightBoxLow,
-                            Center(
-                                child: ElevatedButton(
-                                    onPressed: () {},
-                                    child: Text('Randevu Ekle'))),
-                            context.sized.emptySizedHeightBoxNormal,
-                            Text('Randevu Bulunmamaktadır.'),
-                          ],
+                        // Veri varsa
+                        final List<AppointmentModel> appointments = snapshot.data!.docs.map((doc) {
+
+                          Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+                          return AppointmentModel.fromJson(data);
+                        }).toList();
+
+                        return ListView.builder(
+                            shrinkWrap: true,
+                            physics: ScrollPhysics(),
+                            itemCount: appointments.length,
+                            itemBuilder: (context, int index) {
+
+                              return Card(
+                                margin: context.padding.normal,
+                                child: ListTile(
+                                  title: Text(appointments[index].name.toString()),
+                                ),
+                              );
+                            }
                         );
                       }
                     })
